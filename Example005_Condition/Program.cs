c673d1ca4163daaa6d4a1dd4d8c1e9565de254c5
Vyacheslav V.V <in-lite@list.ru>
2022-09-19 01:27:57 +0500

    Example 005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователья: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ева")
+{
+    Console.WriteLine("Ура, это же Ева!!!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
