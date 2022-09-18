4a0fffc01f4f65cdb3b68b9c7e213e7c4c456ae6
Vyacheslav V.V <in-lite@list.ru>
2022-09-19 01:34:45 +0500

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 5;
+int b = 7;
+int c = 3;
+int d = 2;
+int e = 9;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
